2e9f776cde8c363a7d4cc7dfc4bbbbb02a23175a
gowtham25 <gowthamsumith@gmail.com>
2020-10-02 23:26:03 +0530

    Updates

---
+++ precache-manifest.d243c1c936203e45a1c530144ffe3c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1687e060ca32d9bd54b977cb94a90e6a",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "c5412e4c58d35f632560",
+    "url": "/quiz/static/css/main.13e5c61b.chunk.css"
+  },
+  {
+    "revision": "07bc092fa1e8be42d70f",
+    "url": "/quiz/static/js/2.05926903.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/quiz/static/js/2.05926903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5412e4c58d35f632560",
+    "url": "/quiz/static/js/main.be5735c6.chunk.js"
+  },
+  {
+    "revision": "92182ec1fb6d434d2e02",
+    "url": "/quiz/static/js/runtime-main.d4225ac9.js"
+  }
+]);
